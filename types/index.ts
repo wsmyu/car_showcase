edbe29fef4f221a5456ec9fbf46cb081562a54e9
Wing Suet Michelle Yu <n01535631@humbermail.ca>
2024-04-24 15:22:54 -0400

    inital commit

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps{
+title:String;
+containerStyles?:String;
+handleClick?:MouseEventHandler<HTMLButtonElement>;
+btnType?:"button" | "submit";
+
+}
